a68d8e7b60534b01b1d2eea9a8407d003153b604
ShurjeelCS <shurjeel_m@hotmail.com>
2026-02-12 15:14:59 +0000

    added content to home page through js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // src/index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import turkishFood from "./turkish.jpg";
 
-console.log(greeting);
+console.log(greeting + "baka");
+
+const div = document.querySelector("#content");
+
+const heading = document.createElement("h1");
+heading.textContent = "Turkuaz";
+const paragraph = document.createElement("p");
+paragraph.textContent = "Enjoy authentic turkish food at our affordable fine dining establishment. Our atmostphere provides the perfect environment for group dinners and romantic evenings.";
+const img = document.createElement("img");
+img.src = turkishFood;
+
+div.appendChild(heading);
+div.appendChild(paragraph);
+div.appendChild(img);
